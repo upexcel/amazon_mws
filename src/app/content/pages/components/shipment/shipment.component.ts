f903b36093c4834d24a48a8fee68a8d33c13b639
yuvraj <excellencetechnologies08@gmail.com>
2018-11-05 16:35:45 +0530

    working on shipment changes

--- src/app/content/pages/components/shipment/shipment.component.ts
+++ src/app/content/pages/components/shipment/shipment.component.ts
@@ -3,6 +3,7 @@ import { AuthenticationService } from '../../../../core/auth/authentication.serv
 import { forEach } from '@angular/router/src/utils/collection';
 import { Router } from "@angular/router";
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
 @Component({
   selector: 'm-shipment',
   templateUrl: './shipment.component.html',
@@ -10,32 +11,56 @@ import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 })
 export class ShipmentComponent implements OnInit {
   shipmentList: any;
-  displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
-  dataSource: MatTableDataSource<any>;
+  displayedColumns = [
+    'select',
+    'ShipmentId',
+    'ShipmentName',
+    'ShipmentStatus',
+    'BoxContentsSource',
+    'DestinationFulfillmentCenterId',
+    'LabelPrepType',
+    'ShipFromAddress'
+  ];
   constructor(private _change: ChangeDetectorRef, private _authService: AuthenticationService, private router: Router) { }
-
+  selection = new SelectionModel(true, []);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   ngOnInit() {
     const tableData = [];
     this._authService.getTypeAjax('/seller/ListInboundShipments/A2WR6NEBQYUU6E').subscribe((res) => {
-      this.shipmentList = res['ShipmentData']['member'];
       // console.log("this.shipmentList", this.shipmentList)
-      this.shipmentList.forEach((value: any) => {
-        console.log("*******", { ...value, ...value.ShipFromAddress })
-      })
-      // console.log("this.shipmentList", this.shipmentList)
+      // res['ShipmentData']['member'].forEach((value: any) => {
+      //   console.log("*******", { ...value, ...value.ShipFromAddress })
+      // });
+      this.shipmentList = new MatTableDataSource(res['ShipmentData']['member']);
+      this.shipmentList.paginator = this.paginator;
+      this.shipmentList.sort = this.sort;
       this._change.detectChanges();
-    })
+      console.log('this.shipmentList', this.shipmentList)
+    });
   }
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.shipmentList.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.shipmentList.data.forEach(row => this.selection.select(row));
+  }
+
   gotoInboundShipments(id) {
     this.router.navigate(['inbound', id]);
   }
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+  applyFilter(filterValue: string) {
+    this.shipmentList.filter = filterValue.trim().toLowerCase();
+
+    if (this.shipmentList.paginator) {
+      this.shipmentList.paginator.firstPage();
     }
   }
 }
--- src/app/content/pages/components/shipment/shipment.module.ts
+++ src/app/content/pages/components/shipment/shipment.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ShipmentComponent } from './shipment.component';
 import { RouterModule } from '@angular/router';
 import { LayoutModule } from '../../../layout/layout.module';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { NgxJsonViewerModule } from 'ngx-json-viewer';
 import { MatPaginatorModule } from '@angular/material';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from "@angular/material/icon";
+import {
+	MatSortModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatCheckboxModule
+} from '@angular/material';
 @NgModule({
 	imports: [
 		CommonModule,
@@ -19,6 +25,10 @@ import { MatIconModule } from "@angular/material/icon";
 		MatSelectModule,
 		MatButtonModule,
 		MatIconModule,
+		MatSortModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatCheckboxModule,
 		RouterModule.forChild([
 			{
 				path: '',
